b7701e8dc2c052b3aceaaa70342e095679e1d595
saintabomination <saint.abomination@protonmail.com>
2022-06-09 22:00:46 +0200

    Adding Lato font

--- theme/MainStyle.ts
+++ theme/MainStyle.ts
@@ -1,7 +1,9 @@
 import { createGlobalStyle } from '@xstyled/styled-components';
 
 const MainStyle = createGlobalStyle`
-
+  body {
+    font-family: 'Lato', sans-serif;
+  }
 `;
 
 export default MainStyle;
