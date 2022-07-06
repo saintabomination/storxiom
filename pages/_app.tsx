fe420853e53536efd13ec6d492eb2d78c14a36ea
saintabomination <saint.abomination@protonmail.com>
2022-07-06 17:11:12 +0200

    Adding and setting up xstyled

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { ThemeProvider } from '@xstyled/styled-components';
+
 import type { AppProps } from 'next/app';
 
+import mainTheme from '../theme/mainTheme';
+import ResetStyles from '../theme/resetStyles';
+import MainStyle from '../theme/mainStyle';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <ResetStyles />
+      <MainStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
