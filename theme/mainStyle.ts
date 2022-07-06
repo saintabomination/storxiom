0c744d99f4bdd28055e41a42e3220e66d9065e29
saintabomination <saint.abomination@protonmail.com>
2022-07-06 17:13:36 +0200

    CSS Reset

--- theme/mainStyle.ts
+++ theme/mainStyle.ts
@@ -1,7 +1,19 @@
 import { createGlobalStyle } from '@xstyled/styled-components';
 
 const MainStyle = createGlobalStyle`
+  html {
+    font-size: 56.25%;
+  }
 
+  @media (min-width: 768px) {
+    html {
+      font-size: 62.5%;
+    }
+  }
+
+  body {
+    font-size: 1.4rem;
+  }
 `;
 
 export default MainStyle;
--- theme/resetStyles.ts
+++ theme/resetStyles.ts
@@ -1,7 +1,23 @@
 import { createGlobalStyle } from '@xstyled/styled-components';
 
 const ResetStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: inherit;
+  }
 
+  ul {
+    list-style: none;
+  }
+
+  a {
+    color: unset;
+    text-decoration: none;
+  }
 `;
 
 export default ResetStyles;
